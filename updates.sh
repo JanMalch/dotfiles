35f5f38686b3531254a00364c668477fd70879ad
JanMalch <25508038+JanMalch@users.noreply.github.com>
2026-05-01 19:29:05 +0000

    Add git-jump

--- updates.sh
+++ updates.sh
@@ -11,9 +11,16 @@ update_bat_theme() {
     echo "bat theme updated"
 }
 
+# https://github.com/git/git/blob/master/contrib/git-jump/README
+update_git_jump() {
+    curl -sS -L -o "home/scripts/git-jump" "https://raw.githubusercontent.com/git/git/refs/heads/master/contrib/git-jump/git-jump"
+    echo "git-jump updated"
+}
+
 if [ -z "$(git status --porcelain)" ]; then 
     # Working directory clean
     update_bat_theme
+    update_git_jump
     git status
 else 
     # Uncommitted changes
