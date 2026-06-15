225460e81ee0f1da33be6fa4fdd9e97345a55e4d
JanMalch <25508038+JanMalch@users.noreply.github.com>
2026-06-15 09:03:50 +0000

    Update telescope

--- home/.config/nvim/lua/janmalch/lazy/telescope.lua
+++ home/.config/nvim/lua/janmalch/lazy/telescope.lua
@@ -48,9 +48,7 @@ end
 
 return {
     "nvim-telescope/telescope.nvim",
-
-    tag = "0.1.5",
-
+    tag = "v0.2.2",
     dependencies = {
         "nvim-lua/plenary.nvim",
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
