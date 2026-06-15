c85b1ae74c87bca980ca45f4cb2054c99ae9922d
JanMalch <25508038+JanMalch@users.noreply.github.com>
2026-06-15 08:22:41 +0000

    Use fzf-native for nvim telescope

--- home/.config/nvim/lua/janmalch/lazy/telescope.lua
+++ home/.config/nvim/lua/janmalch/lazy/telescope.lua
@@ -4,11 +4,18 @@ return {
     tag = "0.1.5",
 
     dependencies = {
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
     },
 
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup {
+            extensions = {
+                fzf = {}
+            }
+        }
+
+        require('telescope').load_extension('fzf')
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
