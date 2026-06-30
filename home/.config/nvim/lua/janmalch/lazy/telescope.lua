0369045a5c4ec1489ed1403249a6d437a936aaea
JanMalch <25508038+JanMalch@users.noreply.github.com>
2026-06-30 21:41:36 +0000

    Use ivy theme for more telescope pickers

--- home/.config/nvim/lua/janmalch/lazy/telescope.lua
+++ home/.config/nvim/lua/janmalch/lazy/telescope.lua
@@ -1,90 +1,101 @@
-local pickers = require "telescope.pickers"
-local finders = require "telescope.finders"
-local make_entry = require "telescope.make_entry"
-local conf = require "telescope.config".values
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local make_entry = require("telescope.make_entry")
+local conf = require("telescope.config").values
 
 -- https://github.com/tjdevries/advent-of-nvim/blob/13d4ec68a2a81f27264f3cc73dd7cd8c047aab87/nvim/lua/config/telescope/multigrep.lua
 local live_multigrep = function(opts)
-  opts = opts or {}
-  opts.cwd = opts.cwd or vim.uv.cwd()
+	opts = opts or {}
+	opts.cwd = opts.cwd or vim.uv.cwd()
 
-  local finder = finders.new_async_job {
-    command_generator = function(prompt)
-      if not prompt or prompt == "" then
-        return nil
-      end
+	local finder = finders.new_async_job({
+		command_generator = function(prompt)
+			if not prompt or prompt == "" then
+				return nil
+			end
 
-      local pieces = vim.split(prompt, "  ")
-      local args = { "rg" }
-      if pieces[1] then
-        table.insert(args, "-e")
-        table.insert(args, pieces[1])
-      end
+			local pieces = vim.split(prompt, "  ")
+			local args = { "rg" }
+			if pieces[1] then
+				table.insert(args, "-e")
+				table.insert(args, pieces[1])
+			end
 
-      if pieces[2] then
-        table.insert(args, "-g")
-        table.insert(args, pieces[2])
-      end
+			if pieces[2] then
+				table.insert(args, "-g")
+				table.insert(args, pieces[2])
+			end
 
-      ---@diagnostic disable-next-line: deprecated
-      return vim.tbl_flatten {
-        args,
-        { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case", "--hidden" },
-      }
-    end,
-    entry_maker = make_entry.gen_from_vimgrep(opts),
-    cwd = opts.cwd,
-  }
+			---@diagnostic disable-next-line: deprecated
+			return vim.tbl_flatten({
+				args,
+				{
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+					"--hidden",
+				},
+			})
+		end,
+		entry_maker = make_entry.gen_from_vimgrep(opts),
+		cwd = opts.cwd,
+	})
 
-  pickers.new(opts, {
-    debounce = 100,
-    prompt_title = "Multi Grep",
-    finder = finder,
-    previewer = conf.grep_previewer(opts),
-    sorter = require("telescope.sorters").empty(),
-  }):find()
+	pickers
+		.new(opts, {
+			debounce = 100,
+			prompt_title = "Multi Grep",
+			finder = finder,
+			previewer = conf.grep_previewer(opts),
+			sorter = require("telescope.sorters").empty(),
+		})
+		:find()
 end
 
-
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "v0.2.2",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
-    },
+	"nvim-telescope/telescope.nvim",
+	tag = "v0.2.2",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
 
-    config = function()
-        require('telescope').setup {
-            pickers = {
-                find_files = {
-                    -- open file pickers in a "bottom sheet"
-                    theme = "ivy"
-                }
-            },
-            extensions = {
-                fzf = {}
-            }
-        }
+	config = function()
+		-- open pickers in a "bottom sheet"
+		local pickers_opt = { theme = "ivy" }
+		require("telescope").setup({
+			-- use this to use ivy for everything
+			-- defaults = require("telescope.themes").get_ivy(),
+			pickers = {
+				git_files = pickers_opt,
+				find_files = pickers_opt,
+				buffers = pickers_opt,
+			},
+			extensions = {
+				fzf = {},
+			},
+		})
 
-        require('telescope').load_extension('fzf')
+		require("telescope").load_extension("fzf")
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', function() 
-            builtin.git_files({ show_untracked = true })
-        end, { desc = 'Telescope git files' })
-        vim.keymap.set('n', '<leader>fg', live_multigrep, { desc = 'Telescope live grep' })
-        vim.keymap.set('n', '<leader>fp', builtin.find_files, { desc = 'Telescope project files'})
-        vim.keymap.set('n', '<leader>fw', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>fW', function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-    end
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", function()
+			builtin.git_files({ show_untracked = true })
+		end, { desc = "Telescope git files" })
+		vim.keymap.set("n", "<leader>fg", live_multigrep, { desc = "Telescope live grep" })
+		vim.keymap.set("n", "<leader>fp", builtin.find_files, { desc = "Telescope project files" })
+		vim.keymap.set("n", "<leader>fw", function()
+			local word = vim.fn.expand("<cword>")
+			builtin.grep_string({ search = word })
+		end)
+		vim.keymap.set("n", "<leader>fW", function()
+			local word = vim.fn.expand("<cWORD>")
+			builtin.grep_string({ search = word })
+		end)
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+	end,
 }
-
