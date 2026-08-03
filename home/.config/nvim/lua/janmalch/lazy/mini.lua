226b5b5c02fa4ad85a3c2a903a87910ab793dada
Jan Malchert <25508038+JanMalch@users.noreply.github.com>
2026-08-03 23:46:05 +0200

    Try new Neovim workflow without explorer but tabs and auto-session
    See f53694b6b9c1724a7ae9a30b06a45b5fcb2b6074,
    0f556b894466ea6b91c4565064f80271124bd4af,
    and aa8e8c827830ac096efe8446d37e2c8205f4d7df,
    for previous setups.
    remaps can use some clean ups but let's see what becomes muscle memory.
    Also tried oil.nvim for a second, but I prefer mini.files so far.

--- home/.config/nvim/lua/janmalch/lazy/mini.lua
+++ home/.config/nvim/lua/janmalch/lazy/mini.lua
@@ -1,5 +1,13 @@
 return {
 	{
+		"nvim-mini/mini.files",
+		version = "*",
+		keys = {
+			{ "<leader>e", "<cmd>lua require('mini.files').open(vim.api.nvim_buf_get_name(0))<cr>" },
+			{ "<leader>E", "<cmd>lua require('mini.files').open(nil, false)<cr>" },
+		},
+	},
+	{
 		"nvim-mini/mini.ai",
 		version = "*",
 		config = function()
--- home/.config/nvim/lua/janmalch/remap.lua
+++ home/.config/nvim/lua/janmalch/remap.lua
@@ -44,3 +44,13 @@ vim.keymap.set("n", "<C-p>", "<Cmd>bp<CR>", { silent = true, desc = "[P]revious 
 vim.keymap.set("n", "<C-n>", "<Cmd>bn<CR>", { silent = true, desc = "[N]ext buffer" })
 vim.keymap.set("n", "<C-q>", "<Cmd>bd<CR>", { silent = true, desc = "[Q]uit buffer" })
 vim.keymap.set("n", "<leader><BS>", "<Cmd>e #<CR>", { silent = true, desc = "Alternate buffer" })
+local opts = { noremap = true, silent = true }
+for i = 1, 9 do
+	vim.keymap.set("n", "<leader>" .. i, "<Cmd>BufferGoto " .. i .. "<CR>", opts)
+end
+vim.keymap.set("n", "<leader>0", "<Cmd>BufferLast<CR>", opts)
+vim.keymap.set("n", "<leader>,", "<Cmd>BufferPrevious<CR>", opts)
+vim.keymap.set("n", "<leader>.", "<Cmd>BufferNext<CR>", opts)
+vim.keymap.set("n", "<leader>bp", "<Cmd>BufferPin<CR>", opts)
+vim.keymap.set("n", "<leader>bc", "<Cmd>BufferClose<CR>", opts)
+vim.keymap.set("n", "<leader>bs", "<Cmd>BufferPick<CR>", opts)
